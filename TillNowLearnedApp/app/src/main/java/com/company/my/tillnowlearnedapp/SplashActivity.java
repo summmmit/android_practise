f14a76c6fbed5c248fb5a5fe8d13eed5c52c47e9
Sumit Singh <sumit_prasad@epark.co.jp>
2015-06-03 17:53:39 +0900

    changes to till now read tute
    tute

---
+++ TillNowLearnedApp/app/src/main/java/com/company/my/tillnowlearnedapp/SplashActivity.java
@@ -0,0 +1,71 @@
+package com.company.my.tillnowlearnedapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+
+
+public class SplashActivity extends ActionBarActivity {
+
+    //Set waktu lama splashscreen
+    private static int splashInterval = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+
+
+                //jeda selesai Splashscreen
+                this.finish();
+            }
+
+            private void finish() {
+                // TODO Auto-generated method stub
+
+            }
+        }, splashInterval);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_splash, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
