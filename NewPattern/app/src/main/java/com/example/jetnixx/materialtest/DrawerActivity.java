eadd050395af17b74e60fbba956f8c341b0f88da
Sumit Singh <sumit_prasad@epark.co.jp>
2015-12-03 17:41:15 +0900

    new material design pattern app

---
+++ NewPattern/app/src/main/java/com/example/jetnixx/materialtest/DrawerActivity.java
@@ -0,0 +1,28 @@
+package com.example.jetnixx.materialtest;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class DrawerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawer);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        NavigationDrawerFragment navigationDrawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.navigation_drawer_fragment);
+        navigationDrawerFragment.setUp(drawerLayout, toolbar);
+
+    }
+
+}
--- practiceFinished/MyApplication2/app/src/main/java/com/jetnix/my/simplepagesandfragments/Information.java
+++
@@ -1,9 +0,0 @@
-package com.jetnix.my.simplepagesandfragments;
-
-/**
- * Created by 1084760 on 2015/07/08.
- */
-public class Information {
-    int iconId;
-    String title;
-}
